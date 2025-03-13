c9e3e614488c12c80aa4ee9b62b074c4eccd6678
Sessobin <mohantykrishna57@gmail.com>
2025-03-13 16:53:03 +0530

    methds added

--- Student.java
+++ Student.java
@@ -2,36 +2,71 @@
 //import Package.Student;
 import java.util.Date;
 import java.util.*;
+import java.time.ZoneId;
+import java.time.LocalDate;
 
 abstract class StudentMain {
-    Double Sid;
+    String Sid;
     String SName;
     Date dob;
     String FatherName;
     String Password;
+    LocalDate yearOfJoiningDate;
 
-    StudentMain(Double Sid, String SName, Date dob, String FatherName) {
-        this.Sid = Sid;
+    private static int count = 0;
+
+    StudentMain(String Sid, String SName, Date dob, String FatherName) {
+        this.Sid = generateSid();
         this.SName = SName;
         this.dob = dob;
         this.FatherName = FatherName;
+        this.yearOfJoiningDate = getDate();
         this.Password = generatePassword();
+    }
+
+    protected static void total() {
+        count++;
+    }
+
+    public static int getTotal() {
+        return count;
     }
 
     private String generatePassword() {
         return UUID.randomUUID().toString().substring(0, 8);
     }
 
+    private LocalDate getDate() {
+        Date currDate = new Date();
+        LocalDate localDate = currDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        return localDate;
+    }
+
+    private String generateSid() {
+        String date = getDate().toString();
+        String[] year = date.split("-");
+        String id = (year[0] + (Math.floor(Math.random() * 9) + 1) + count).toString();
+        return id;
+
+    };
+
 }
 
 interface StudOperation {
     abstract void addStudent();
+
     abstract void viewStudents();
+
     abstract void searchStudent();
+
     abstract void updateStudent();
+
     abstract void deleteStudent();
+
     abstract void sortStudent();
+
     abstract void exportCSV();
+
     abstract void cgpaCalc();
 }
 
@@ -42,21 +77,20 @@ class StudentInfo extends StudentMain {
     String Subject;
     Double ContactNo;
     Float CGPA;
-    Date yearOfJoiningDate;
 
-    StudentInfo(Double Sid, String SName, Date dob, String FatherName, String Address, String Degree, String Subject, Double ContactNo, Date yearOfJoiningDate) {
+    StudentInfo(String Sid, String SName, Date dob, String FatherName, String Address, String Degree, String Subject,Double ContactNo) {
         super(Sid, SName, dob, FatherName);
         this.Address = Address;
         this.Degree = Degree;
         this.Subject = Subject;
         this.ContactNo = ContactNo;
-        this.CGPA = 0.0f; 
-        this.yearOfJoiningDate = yearOfJoiningDate;
+        this.CGPA = 0.0f;
+
     }
 }
 
-class Student{
+class Student {
     public static void main(String[] args) {
-        
+
     }
 }
