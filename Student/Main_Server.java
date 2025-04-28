6f5c8c38bf81144421fc7ab6b554f1afc4c105a5
Nipun Kumar Ranjan <nkr.062005@gmail.com>
2025-04-28 13:24:51 +0530

    Main Server for admin and Student login

---
+++ Student/Main_Server.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Main_Server
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int ch;
+        while(true)
+        {
+            System.out.println("Hello There, enter your role.\n1 : For server admin\n2 : For student login\n3 : To exit");
+            ch=sc.nextInt();
+            try
+            {
+            switch (ch) 
+            {
+                case 1:
+                    Student.main(args);
+                    break;
+                case 2:
+                    login.main(args);
+                    break;
+                case 3:
+                    System.out.println("Thank you. Have a nice day ahead");
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+            }
+            catch(AssertionError e)
+            {
+                System.err.println(e.getMessage());
+            }
+            if(ch==3)
+                break;
+        }
+    }   
+}
+
